755a0ad98206a0dca68a0f4c75bf6839b2b3262f
Sinan <me@wunnle.com>
2019-04-23 19:47:23 +0300

    added shadow levels again

--- src/components/ShadowBoard/index.js
+++ src/components/ShadowBoard/index.js
@@ -2,17 +2,18 @@ import React from 'react';
 import { connect } from 'react-redux'
 import styles from './ShadowBoard.module.scss'
 
+const shadowLevels = [styles.shadowLevel1, styles.shadowLevel2, styles.shadowLevel3]
 
 const ShadowBoard = ({ board }) => (
   <div className={styles.shadowBoard}>
     {
-      board[0] && board[0].map(tile => <Tile>{tile.level > 0 && <Shadow/>}</Tile>)
+      board[0] && board.map(row => row.map(tile => <Tile>{tile.level > 0 && <Shadow level={tile.level}/>}</Tile>))
     }
   </div>
 )
 
-const Shadow = () => (
-  <div className={styles.shadow}></div>
+const Shadow = ({level}) => (
+  <div className={[styles.shadow, shadowLevels[level - 1]].join(' ')}></div>
 )
 
 const Tile = ({ children }) => (
