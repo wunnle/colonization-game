15ec8b5ef1ad46fb8e07a5a814f38fe21675340b
wunnle <me@wunnle.com>
2019-06-29 09:23:11 +0300

    minor refactoring

--- src/components/Tile/index.js
+++ src/components/Tile/index.js
@@ -1,3 +1,44 @@
-import Tile from './Tile'
+import React, { Component, useState } from 'react';
+import styles, { tile, tileHolder, sideTopOpaque, side1, side2, side3, side4, sideTop, buildingItself, shadow } from './Tile.module.scss'
+import { Building } from '../Building';
+import PropTypes from 'prop-types'
 
-export default Tile
+
+const Tile = ({ level = 0, owner, handleClick, shadowLevel, zPosition, lightedUp }) => {
+  return (
+    <TileHolder handleClick={handleClick} shadowLevel={shadowLevel} lightedUp={lightedUp} zPosition={zPosition}>
+      <Building owner={owner} level={level} shadowLevel={shadowLevel} isTopTransparent={!zPosition > 0} />
+    </TileHolder>
+  )
+}
+
+Tile.propTypes = {
+    shadowLevel: PropTypes.number,
+    level: PropTypes.number
+}
+
+const shadowLevels = [styles.shadowLevel0, styles.shadowLevel1, styles.shadowLevel2, styles.shadowLevel3]
+
+const TileHolder = ({ children, handleClick, shadowLevel, zPosition, lightedUp }) => {
+
+  const [z, setZ] = useState(zPosition)
+
+
+  return (
+    <div className={`${tile} ${shadowLevels[shadowLevel]}`} onClick={handleClick}
+      style={{
+        transform: `translateZ(${zPosition}px)`,
+        transitionDuration: `${z / 160 - 3.4}s`,
+      }}>
+      {children}
+      <div className={lightedUp ? sideTop : sideTopOpaque}></div>
+      <div className={side1}></div>
+      <div className={side2}></div>
+      <div className={side3}></div>
+      <div className={side4}></div>
+      <div className={shadow}></div>
+    </div>
+  )
+}
+
+export default React.memo(Tile)
