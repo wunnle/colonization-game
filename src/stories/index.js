913cd1e17ac960e237495d81673a716ad92061ea
wunnle <me@wunnle.com>
2019-06-30 19:47:30 +0300

    complete Planet control on storybook

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Button } from '@storybook/react/demo';
 import Tile from '../components/Tile'
 import Planet from '../components/Planet';
 import Provider from '../Provider'
@@ -42,17 +41,32 @@ const optionsObj = {
 storiesOf('Planet', module)
   .addDecorator(withProvider)
   .addDecorator(withKnobs)
-  .add('default', () => (
-    <>
-      <div className="App">
-        <Planet
-          sunDirection={optionsKnob('sunDirection', lightOpts, 'right', optionsObj)}
-          lightedUp={true}
-          bricksFallen={true}
-        >
-          {Array(number('count', 4)).fill(<Tile lightedUp={true} level={number('level', 0)} owner={optionsKnob('owner', ownerOpts, '', optionsObj)} shadowLevel={0} />)}
-        </Planet>
-      </div>
-    </>
-  ))
+  .add('default', () => {
 
+    const noOpts = {
+      range: false,
+      min: 1,
+      max: 20,
+      step: 1,
+   };
+
+    const numberOfRows = number('rows', 3, noOpts);
+    const numberOfCols = number('cols', 1, noOpts);
+    const count = numberOfRows * numberOfCols
+
+    return (
+      <>
+        <div className="App">
+          <Planet
+            sunDirection={optionsKnob('sunDirection', lightOpts, 'right', optionsObj)}
+            lightedUp={true}
+            bricksFallen={true}
+            numberOfRows={numberOfRows}
+            numberOfColumns={numberOfCols}
+          >
+            {Array(count).fill(<Tile lightedUp={true} level={number('level', 0)} owner={optionsKnob('owner', ownerOpts, '', optionsObj)} shadowLevel={0} />)}
+          </Planet>
+        </div>
+      </>
+    )
+  })
